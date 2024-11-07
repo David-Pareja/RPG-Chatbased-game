6289b92479abb39fd86683d02f3ebfaa0ff3ab64
David Pareja <davidpareja2004@gmail.com>
2024-11-07 11:51:10 -0500

    RPG Chat base game

---
+++ Database.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGAdv
+{
+    internal class Database
+    {
+        public SQLiteConnection myConnection;
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data source=C:\\Users\\Water\\source\\repos\\rpgADV\\rpgADV\\bin\\Debug\\database.sql3");
+
+            if (!File.Exists("./database.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite3");
+                System.Console.WriteLine("Database file created");
+            }
+        }
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (myConnection.State == System.Data.ConnectionState.Open)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+namespace RPGAdv
+{
+    class Global
+    {
+        public static int checkpoint;
+        public static bool loadedCheckPoint;
+        public static string name;
+
+        class Program
+        {
+            static void Main()
+            {
+                Database databaseObject =  new Database();
+
+                Console.WriteLine("Welcome Voyager! Have you played this game before?");
+                string playedInput = Console.ReadLine();
+
+                if (playedInput == "No")
+                {
+                    //Create profile
+                    Console.WriteLine("Welcome new player! Please start by telling me your name!");
+                    string name = Console.ReadLine();
+
+                    //Tell data base what we want it to do
+                    string query = "INSERT INTO login ('name', 'checkpoint') VALUES (@name, @checkpoint)";
+
+                    //Creates a SQLite coomand
+                    SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+
+                    //OPens the databse
+                    databaseObject.OpenConnection();
+
+                    //Insert the data into the database1name);
+                    myCommand.Parameters.AddWithValue("@checkpoint", 0);
+
+                    //Execute the command
+                    myCommand.ExecuteNonQuery();
+
+                    //Close the data
+                    databaseObject.CloseConnection();
+
+                    Console.WriteLine($"Welcome {name}! Allow me a moment to set up your profile");
+
+                    //Connect to the pause method
+                    Pause();
+
+                    Console.WriteLine("Ready! Press enter to begin");
+                    Console.ReadLine();
+
+                    //Connect to the StartGame Method
+                    StartGame();
+
+                    Console.ReadKey();
+                }
+
+                else if (playedInput == "Yes")
+                {
+                    Console.WriteLine("Please tell me your name");
+                    Global.name = Console.ReadLine();
+
+                    Console.WriteLine("Great! Give me a moment to pull up your profile.");
+                    Pause();
+
+
+                    // checking user info form the database.
+
+                    string query = $"SELECT * FROM login WHERE name = '{Global.name}'";
+
+                    SQLiteCommand myCommand = new SQLiteCommand(query, databaseObject.myConnection);
+
+                    databaseObject.OpenConnection();
+
+                    SQLiteDataReader user = myCommand.ExecuteReader();
+
+                    // checking to make sure the database has info in it.
+
+                    if (user.HasRows)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
